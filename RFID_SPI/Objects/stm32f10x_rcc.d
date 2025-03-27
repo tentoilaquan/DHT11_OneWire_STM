44f133a5996462bfb873a58a01dc49dfa423085b
tentoilaquan <nobitusan@gmail.com>
2025-03-27 23:49:18 +0700

    first commit

---
+++ RFID_SPI/Objects/stm32f10x_rcc.d
@@ -0,0 +1,15 @@
+./objects/stm32f10x_rcc.o: \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\src\stm32f10x_rcc.c \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  D:\File_bai_tap\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  D:\File_bai_tap\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  D:\File_bai_tap\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  D:\File_bai_tap\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  D:\File_bai_tap\Keil_v5\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
+  RTE\Device\STM32F103C8\stm32f10x_conf.h \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_spi.h \
+  D:\File_bai_tap\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\misc.h
